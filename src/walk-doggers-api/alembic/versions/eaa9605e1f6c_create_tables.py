cfad39bbd2efd84907ff032e97b3668577386031
Domen Bukovac <domen@bukovac.si>
2021-05-02 23:48:23 +0200

    moving procfile

---
+++ src/walk-doggers-api/alembic/versions/eaa9605e1f6c_create_tables.py
@@ -0,0 +1,24 @@
+"""create tables
+
+Revision ID: eaa9605e1f6c
+Revises: ba793f8908b7
+Create Date: 2021-05-02 23:20:47.281645
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eaa9605e1f6c'
+down_revision = 'ba793f8908b7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
