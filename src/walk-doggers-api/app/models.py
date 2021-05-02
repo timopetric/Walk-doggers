71fa4ea5eab2992eb5ee6c36167352e445755b41
Domen Bukovac <domen@bukovac.si>
2021-05-02 19:01:33 +0200

    fastapi basic setup + postgresql + alembic + docker

---
+++ src/walk-doggers-api/app/models.py
@@ -0,0 +1,11 @@
+from uuid import uuid4
+
+from sqlalchemy import Column, String, Text
+from sqlalchemy.dialects.postgresql import UUID
+
+from .db.base import Base
+
+
+class Post(Base):
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid4)
+    title = Column(String)
