4cbaa65db02fc66923a3d97049f0c59b20de860c
FriGithub <mg6004@student.uni-lj.si>
2021-05-07 17:21:23 +0200

    basic settings view

--- src/walk-doggers-app/screens/TabSettings.tsx
+++ src/walk-doggers-app/screens/TabSettings.tsx
@@ -1,36 +1,66 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from 'react-native';
-import {Entypo} from "@expo/vector-icons";
-import useColorScheme from "../hooks/useColorScheme";
+import { AntDesign, Ionicons, FontAwesome5, FontAwesome } from '@expo/vector-icons';
+import {Card} from "react-native-elements";
+
+const mainColor = '#303030';
 
 export default function TabSettings() {
-  const colorScheme = useColorScheme();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Settings</Text>
+      <Card wrapperStyle={styles.cardWrapper} containerStyle={styles.cardContainer}>
+        <View style={{flexDirection: 'row'}}>
+          <Ionicons size={30} name="person" color={mainColor} />
+          <Text style={{marginLeft: 5}}>Profile</Text>
+        </View>
+        <AntDesign onPress={() => alert('TODO implement')} size={30} name="right" color={mainColor} style={{justifyContent: 'flex-end'}} />
+      </Card>
 
-      
+      <Card wrapperStyle={styles.cardWrapper} containerStyle={styles.cardContainer}>
+        <View style={{flexDirection: 'row'}}>
+          <FontAwesome5 size={30} name="dog" color={mainColor} />
+          <Text style={{marginLeft: 5}}>My Dogs</Text>
+        </View>
+        <AntDesign onPress={() => alert('TODO implement')} size={30} name="right" color={mainColor} style={{justifyContent: 'flex-end'}} />
+      </Card>
+
+      <Card wrapperStyle={styles.cardWrapper} containerStyle={styles.cardContainer}>
+        <View style={{flexDirection: 'row'}}>
+          <FontAwesome size={30} name="newspaper-o" color={mainColor} />
+          <Text style={{marginLeft: 5}}>Become a Reporter</Text>
+        </View>
+        <AntDesign onPress={() => alert('TODO implement')} size={30} name="right" color={mainColor} style={{justifyContent: 'flex-end'}} />
+      </Card>
+
+      <Card wrapperStyle={styles.cardWrapper} containerStyle={styles.cardContainer}>
+        <View style={{flexDirection: 'row'}}>
+          <Ionicons size={30} name="exit-outline" color={mainColor} />
+          <Text style={{marginLeft: 5}}>Logout</Text>
+        </View>
+        <AntDesign onPress={() => alert('TODO implement')} size={30} name="right" color={mainColor} style={{justifyContent: 'flex-end'}} />
+      </Card>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  cardContainer: {
+    width: '80%',
+  },
+  cardWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
