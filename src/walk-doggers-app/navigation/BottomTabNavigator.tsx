fc7502c54de9ef3e148b039d0077495509512c33
FriGithub <mg6004@student.uni-lj.si>
2021-05-07 08:51:19 +0200

    Bottom navigation icons

--- src/walk-doggers-app/navigation/BottomTabNavigator.tsx
+++ src/walk-doggers-app/navigation/BottomTabNavigator.tsx
@@ -4,6 +4,8 @@
  */
 
 import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
@@ -26,85 +28,82 @@ import TabSettings from "../screens/TabSettings";
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
 export default function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <BottomTab.Navigator
-      initialRouteName="Explore"
-      tabBarOptions={{ activeTintColor: '#eee' }}>
-        <BottomTab.Screen
-            name="Explore"
-            component={ExploreNavigator}
-            options={{
-              tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-            }}
-        />
-        <BottomTab.Screen
-            name="Inbox"
-            component={InboxNavigator}
-            options={{
-              tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-            }}
-        />
-        <BottomTab.Screen
-            name="Blog"
-            component={BlogNavigator}
-            options={{
-                tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-            }}
-        />
-        <BottomTab.Screen
-            name="Listings"
-            component={ListingsNavigator}
-            options={{
-                tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-            }}
-        />
-        <BottomTab.Screen
-            name="Settings"
-            component={SettingsNavigator}
-            options={{
-                tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-            }}
-        />
-    </BottomTab.Navigator>
-  );
+    return (
+        <BottomTab.Navigator
+            initialRouteName="Explore"
+            tabBarOptions={{ activeTintColor: '#854dbd' }}>
+            <BottomTab.Screen
+                name="Explore"
+                component={ExploreNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <Ionicons size={30} style={{ marginBottom: -3 }} name="search" color={color}/>,
+                }}
+            />
+            <BottomTab.Screen
+                name="Inbox"
+                component={InboxNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <MaterialIcons size={30} style={{ marginBottom: -3 }} name="message" color={color} />,
+                }}
+            />
+            <BottomTab.Screen
+                name="Blog"
+                component={BlogNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <Entypo size={30} style={{ marginBottom: -3 }} name="text" color={color} />,
+                }}
+            />
+            <BottomTab.Screen
+                name="Listings"
+                component={ListingsNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <Entypo size={30} style={{ marginBottom: -3 }} name="folder-images" color={color} />,
+                }}
+            />
+            <BottomTab.Screen
+                name="Settings"
+                component={SettingsNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <Ionicons size={30} style={{ marginBottom: -3 }} name="person-circle-outline" color={color} />,
+                }}
+            />
+        </BottomTab.Navigator>
+    );
 }
 
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
-function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-}
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
 const ExploreStack = createStackNavigator<ExploreParamList>();
 
 function ExploreNavigator() {
-  return (
-    <ExploreStack.Navigator>
-      <ExploreStack.Screen
-        name="ExploreScreen"
-        component={TabExplore}
-        options={{ headerTitle: 'Explore Title' }}
-      />
-    </ExploreStack.Navigator>
-  );
+    return (
+        <ExploreStack.Navigator>
+            <ExploreStack.Screen
+                name="ExploreScreen"
+                component={TabExplore}
+                options={{ headerTitle: 'Explore Title' }}
+            />
+        </ExploreStack.Navigator>
+    );
 }
 
 const InboxStack = createStackNavigator<InboxParamList>();
 
 function InboxNavigator() {
-  return (
-    <InboxStack.Navigator>
-      <InboxStack.Screen
-        name="InboxScreen"
-        component={TabInbox}
-        options={{ headerTitle: 'Inbox Title' }}
-      />
-    </InboxStack.Navigator>
-  );
+    return (
+        <InboxStack.Navigator>
+            <InboxStack.Screen
+                name="InboxScreen"
+                component={TabInbox}
+                options={{ headerTitle: 'Inbox Title' }}
+            />
+        </InboxStack.Navigator>
+    );
 }
 
 const BlogStack = createStackNavigator<BlogParamList>();
