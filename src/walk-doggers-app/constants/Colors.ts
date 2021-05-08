ad5a246920292b6c6f4b2ad2c4772b452269529b
Domen Bukovac <domen@bukovac.si>
2021-05-08 14:45:46 +0200

    design colors constants

--- src/walk-doggers-app/constants/Colors.ts
+++ src/walk-doggers-app/constants/Colors.ts
@@ -1,6 +1,22 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
+const PRIMARY = '#8265F3';
+const PRIMARY_DARK = '#6951C7';
+const PRIMARY_DARKER = '#5B3ADE';
+
+const GRAY_0 = '#EDEDED';
+const GRAY_1 = '#C4C4C4';
+const GRAY_2 = '#A3A3A3';
+const GRAY_3 = '#676767';
+
+const YELLOW = '#FFDD7D';
+const GREEN = '#6FC268';
+const RED = '#ED4C5C';
+const PINK = '#F365AA';
+const ORANGE = '#FFA25F';
+const BLUE = '#2878EF';
+
 export default {
   light: {
     text: '#000',
