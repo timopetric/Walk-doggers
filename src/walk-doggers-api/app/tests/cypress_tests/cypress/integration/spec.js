bba9847fdd4e24c115d0c1c6f230db62a10c6b9b
timopetric <timopetric@hotmail.com>
2021-05-10 01:27:13 +0200

    fix cypress test run, disable screenshots and video

--- src/walk-doggers-api/app/tests/cypress_tests/cypress/integration/spec.js
+++ src/walk-doggers-api/app/tests/cypress_tests/cypress/integration/spec.js
@@ -1,11 +1,12 @@
 describe('My First Test', () => {
   it('Finds Husky string in page', () => {
-    cy.visit("http://localhost:19006")
+    cy.visit("http://localhost:19006/")
+    cy.contains("Login").click()
     cy.contains("Husky")
   })
 
   it('Doesent find Husssky string in page', () => {
-    cy.visit("http://localhost:19006")
+    cy.visit("http://localhost:19006/explore")
     cy.contains("Husssky")
   })
 
