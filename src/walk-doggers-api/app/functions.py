3edf2d4c7659c12c54a0851cf84d9b1dc37ac705
timopetric <timopetric@hotmail.com>
2021-05-13 00:09:10 +0200

    add conversation and message models and create the conversation api

--- src/walk-doggers-api/app/functions.py
+++ src/walk-doggers-api/app/functions.py
@@ -1,4 +1,10 @@
 from typing import Generator
+
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from starlette.status import HTTP_404_NOT_FOUND
+
+from app.postgres import actions
 from app.postgres.session import SessionLocal
 
 
@@ -13,3 +19,12 @@ def get_db() -> Generator:
 regex_password = r"^(?=.*[A-Z].*)(?=.*[!@#$&*])(?=.*[0-9].*)(?=.*[a-z].*[a-z].*[a-z]).{8,}$"
 regex_email = r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[" \
               r"a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+ERROR_USER_NOT_FOUND = "User with id: {} not found in the database"
+
+
+def get_user_from_id(db: Session, user_id: str):
+    """get an user object from db session and return it"""
+    user = actions.user.get(db=db, id=user_id)
+    if user is None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=ERROR_USER_NOT_FOUND.format(user_id))
+    return user
