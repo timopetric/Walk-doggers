e63c4ee3bc10895f0e32b2e5df5a2c526b6cf233
Domen Bukovac <domen@bukovac.si>
2021-05-19 20:03:03 +0200

    edit profile restricted max width for web

--- src/walk-doggers-app/screens/Settings/EditProfileScreen.tsx
+++ src/walk-doggers-app/screens/Settings/EditProfileScreen.tsx
@@ -18,17 +18,20 @@ export default function EditProfileScreen({navigation}: any) {
     const [imageUrl, setImageUrl] = useState('https://beta.finance.si//pics//cache_ch/challe-salle-foto-bruno-sedevcic-5b40a7709a46f.jpg.cut.1530963962-5b40a7fa5a7dc.jpg')
 
     return (
-        <ScrollView style={styles.container}>
-            <View style={{alignItems: "center", justifyContent: "center", height: 150}}>
-                <ImageUpload circle={true} size={100} showEdit={true}
-                             defaultUrl={imageUrl} saveUrl={setImageUrl}/>
+        <ScrollView style={styles.container} contentContainerStyle={{alignItems: "center",}}>
+            <View style={styles.innerContainer}>
+
+                <View style={{alignItems: "center", justifyContent: "center", height: 150}}>
+                    <ImageUpload circle={true} size={100} showEdit={true}
+                                 defaultUrl={imageUrl} saveUrl={setImageUrl}/>
+                </View>
+                <FormItem label={"FIRST NAME"} placeholder={"Enter your first name"}/>
+                <FormItem label={"LAST NAME"} placeholder={"Enter your last name"}/>
+                <FormItem label={"EMAIL"}/>
+                <FormItem label={"ABOUT ME"} placeholder={"Describe yourself"} height={120}/>
+                <ButtonCustom text='Save changes' onPress={() => onPressSaveChanges(navigation)} color="purple"
+                              style={{marginBottom: 20}}/>
             </View>
-            <FormItem label={"FIRST NAME"} placeholder={"Enter your first name"}/>
-            <FormItem label={"LAST NAME"} placeholder={"Enter your last name"}/>
-            <FormItem label={"EMAIL"}/>
-            <FormItem label={"ABOUT ME"} placeholder={"Describe yourself"} height={120}/>
-            <ButtonCustom text='Save changes' onPress={() => onPressSaveChanges(navigation)} color="purple"
-                          style={{marginBottom: 20}}/>
         </ScrollView>
 
     );
@@ -39,6 +42,11 @@ const styles = StyleSheet.create({
         // margin: 20,
         paddingHorizontal: 20,
         backgroundColor: "white",
+        flex: 1
+    },
+    innerContainer: {
+        width: 500,
+        maxWidth: "100%",
         flex: 1
     },
     imageRow: {
