3884e4fcd57c94f8a91630024da1a685087f9fda
timopetric <timopetric@hotmail.com>
2021-05-21 18:58:33 +0200

    add moderator blog rejection deletion through PUT request, add administrator blog deletion check to see if moderator has approved the blog post yet

--- src/walk-doggers-api/app/routes/blog.py
+++ src/walk-doggers-api/app/routes/blog.py
@@ -3,7 +3,8 @@ from typing import Any, List
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import UUID4
 from sqlalchemy.orm import Session
-from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
+from starlette.responses import Response
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_204_NO_CONTENT, HTTP_403_FORBIDDEN
 
 from app.auth import AuthHandler
 from app.postgres import actions
@@ -37,7 +38,9 @@ def create_blog_post(*, db: Session = Depends(get_db), post_in: schemas.BlogPost
 @BlogRouter.put(
     "/{id}",
     response_model=schemas.BlogPost,
-    responses={HTTP_404_NOT_FOUND: {"model": schemas.HTTPError}},
+    responses={HTTP_404_NOT_FOUND: {"model": schemas.HTTPError},
+               HTTP_204_NO_CONTENT: {"model": {}, "description": "Moderator successfully rejected and with that "
+                                                                 "deleted the blog post."}},
     dependencies=[Depends(auth_handler.is_moderator)]
 )
 def update_blog_post(
@@ -46,19 +49,32 @@ def update_blog_post(
     blog_post = actions.blog_post.get(db=db, id=id)
     if not blog_post:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Post not found")
-    blog_post = actions.blog_post.update(db=db, db_obj=blog_post, obj_in=post_in)
+
+    if not post_in.approved:
+        # if post is not approved by moderator, then it will be deleted - 204 NO CONTENT returned
+        actions.blog_post.remove(db=db, id=blog_post.id)
+        return Response(status_code=HTTP_204_NO_CONTENT)
+    else:
+        # if post is approved, update it with possible modifications from moderator and return it
+        blog_post = actions.blog_post.update(db=db, db_obj=blog_post, obj_in=post_in)
     return blog_post
 
 
 @BlogRouter.delete(
     "/{id}",
     response_model=schemas.BlogPostDeleted,
-    responses={HTTP_404_NOT_FOUND: {"model": schemas.HTTPError}},
-    dependencies=[Depends(auth_handler.is_admin)]
+    responses={HTTP_204_NO_CONTENT: {"model": {}, "description": "Administrator successfully deleted the blog post."}},
+    dependencies=[Depends(auth_handler.is_admin)],
+    description="If the blog post has attribute approved set to true, administrator can delete it"
 )
 def delete_blog_post(*, db: Session = Depends(get_db), id: UUID4) -> Any:
     blog_post = actions.blog_post.get(db=db, id=id)
     if not blog_post:
-        raise HTTP_404_NOT_FOUND()
-    blog_post = actions.blog_post.remove(db=db, id=id)
-    return blog_post
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+
+    if blog_post.approved:
+        actions.blog_post.remove(db=db, id=id)
+    else:
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="Error, moderator must approve the blog post first.")
+
+    return Response(status_code=HTTP_204_NO_CONTENT)
