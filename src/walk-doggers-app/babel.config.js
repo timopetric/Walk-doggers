f3f09dfd9202a997df7f4f28c8b23cb18391d62b
Domen Bukovac <domen@bukovac.si>
2021-05-22 01:19:49 +0200

    babel config env try 33

--- src/walk-doggers-app/babel.config.js
+++ src/walk-doggers-app/babel.config.js
@@ -2,6 +2,8 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        plugins: ['inline-dotenv'],
+        "plugins": [["inline-dotenv", {
+            path: '.env' // See motdotla/dotenv for more options
+        }]]
     };
 };
