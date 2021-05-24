46aeaf77132b73f6fa88673df3c4005729e5e39f
timopetric <timopetric@hotmail.com>
2021-05-24 03:06:05 +0200

    enale backend api on CI

--- src/walk-doggers-app/localConstants.js
+++ src/walk-doggers-app/localConstants.js
@@ -1,2 +1,2 @@
-// export const BASE_API_URL = 'http://localhost';
-export const BASE_API_URL = 'https://walk-doggers.herokuapp.com';
+export const BASE_API_URL = 'http://localhost';
+// export const BASE_API_URL = 'https://walk-doggers.herokuapp.com';
